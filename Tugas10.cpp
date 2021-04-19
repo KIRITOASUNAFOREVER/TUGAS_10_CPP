b946fae060ac2045c917f5dbf8b4fd4970fd3d7d
EDWARD <60771749+KiritoEdward@users.noreply.github.com>
2021-04-19 10:27:08 +0700

    Add files via upload

---
+++ Tugas10.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+void selamat_datang(){
+    cout << "Selamat Datang Di ";
+}
+    
+void perkalian(){
+    cout << "Fungsi Perkalian" << endl;
+}
+
+int main(){
+    selamat_datang();
+    perkalian();
+    return 0;
+}
